06d52dd44627c3f1919f1a9f71118970bef88587
Amit Novick <axnovick@gmail.com>
2019-08-01 00:03:29 +0300

    ExplorerScreen: Tweaked CategoryModal header

--- app/renderer/screens/GraphExplorerScreen/CategoryRenameModalWidget/components/RenameModalHeader.js
+++ app/renderer/screens/GraphExplorerScreen/CategoryRenameModalWidget/components/RenameModalHeader.js
@@ -7,7 +7,7 @@ const RenameModalHeader = ({ category }) => {
   return (
     <Modal.Header>
       <Header as="h3" textAlign="center">
-        {category === null ? '' : `Rename ${category.name}`}
+        {category === null ? '' : `Rename "${category.name}"`}
       </Header>
     </Modal.Header>
   );
