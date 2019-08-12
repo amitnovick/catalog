296ec2109da5916453a67e0a2ba61020482d988c
Amit Novick <axnovick@gmail.com>
2019-08-12 12:18:01 +0300

    Bugfix: opening file with preferred application on Windows

--- app/renderer/fs/openFileByName.js
+++ app/renderer/fs/openFileByName.js
@@ -1,26 +1,10 @@
 import formatFilePath from './formatFilePath';
 
-const childProcess = require('child_process');
-
-const getOpenProgramName = () => {
-  switch (process.platform) {
-    case 'darwin':
-      return 'open';
-    case 'win32':
-      return 'start';
-    case 'win64':
-      return 'start';
-    default:
-      return 'xdg-open';
-  }
-};
+import { shell } from 'electron';
 
 const openFileByName = (fileName) => {
   const filePath = formatFilePath(fileName);
-  const openProgramProcess = childProcess.exec(`${getOpenProgramName()} "${filePath}"`);
-  openProgramProcess.stderr.on('data', (data) => {
-    console.log('Error while trying to open file:', data);
-  });
+  shell.openItem(filePath);
 };
 
 export default openFileByName;
