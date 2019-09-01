78c2b08f426f34485d0a3350bc5e57a1e6d5c1f1
Amit Novick <axnovick@gmail.com>
2019-09-01 22:14:38 +0300

    1. Removed leftover cruft 2. Bumped version number for release

--- app/renderer/components/CategoryIcon.js
+++ app/renderer/components/CategoryIcon.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { faSitemap } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import SvgSvgCategoryIcon from './icons/SvgCategoryIcon';
 
 const CategoryIcon = (props) => {
