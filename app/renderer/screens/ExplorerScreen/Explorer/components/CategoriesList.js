b5804c920181031445bdada593b7e73beb14f73b
Amit Novick <axnovick@gmail.com>
2019-09-03 14:02:13 +0300

    Refactoring: removed unnecessary logic from windowed CategoriesList implementation

--- app/renderer/screens/ExplorerScreen/Explorer/components/CategoriesList.js
+++ app/renderer/screens/ExplorerScreen/Explorer/components/CategoriesList.js
@@ -7,34 +7,6 @@ import AutoSizer from 'react-virtualized-auto-sizer';
 import CategoryListItem from './CategoryListItem';
 import { CATEGORY_LIST_ITEM_HEIGHT } from './layoutConstants';
 
-const CategoryItemContext = React.createContext();
-
-const Row = ({ index, style, data }) => {
-  const category = data[index];
-
-  return (
-    <CategoryItemContext.Consumer>
-      {({ hasSelectedRow, selectedCategoryRow, onClickRow }) => (
-        <div
-          style={{
-            ...style,
-            pointerEvents: 'auto',
-          }}>
-          <CategoryListItem
-            category={category}
-            isSelected={
-              hasSelectedRow &&
-              selectedCategoryRow !== null &&
-              selectedCategoryRow.id === category.id
-            }
-            onClickRow={onClickRow}
-          />
-        </div>
-      )}
-    </CategoryItemContext.Consumer>
-  );
-};
-
 const CategoriesList = ({
   categories,
   hasSelectedRow,
@@ -45,28 +17,38 @@ const CategoriesList = ({
   return (
     <>
       {categories.length > 0 ? (
-        <CategoryItemContext.Provider
-          value={{
-            hasSelectedRow: hasSelectedRow,
-            selectedCategoryRow: selectedCategoryRow,
-            onClickRow: onClickRow,
-          }}>
-          <AutoSizer>
-            {({ width, height }) => (
-              <WindowedList
-                itemData={categories}
-                ref={listRef}
-                className="List"
-                height={height}
-                width={width}
-                itemCount={categories.length}
-                itemSize={CATEGORY_LIST_ITEM_HEIGHT}
-                style={{ padding: categories.length === 0 ? '0.5em' : 0 }}>
-                {Row}
-              </WindowedList>
-            )}
-          </AutoSizer>
-        </CategoryItemContext.Provider>
+        <AutoSizer>
+          {({ width, height }) => (
+            <WindowedList
+              ref={listRef}
+              className="List"
+              height={height}
+              width={width}
+              itemCount={categories.length}
+              itemSize={CATEGORY_LIST_ITEM_HEIGHT}>
+              {({ index, style }) => {
+                const category = categories[index];
+                return (
+                  <div
+                    style={{
+                      ...style,
+                      pointerEvents: 'auto',
+                    }}>
+                    <CategoryListItem
+                      category={category}
+                      isSelected={
+                        hasSelectedRow &&
+                        selectedCategoryRow !== null &&
+                        selectedCategoryRow.id === category.id
+                      }
+                      onClickRow={onClickRow}
+                    />
+                  </div>
+                );
+              }}
+            </WindowedList>
+          )}
+        </AutoSizer>
       ) : (
         <div style={{ padding: '0.5em' }}>
           <Message info>
